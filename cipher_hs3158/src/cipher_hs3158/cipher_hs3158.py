c6ca1d3caced9a6a5b3a87a9b6fb72d9f994b212
Alatas <ha2573@cumc.columbia.edu>
2021-11-09 11:26:26 -0500

    'cipher'

--- cipher_hs3158/src/cipher_hs3158/cipher_hs3158.py
+++ cipher_hs3158/src/cipher_hs3158/cipher_hs3158.py
@@ -1,6 +1,28 @@
 import pandas as pd
 
 def cipher(text, shift, encrypt=True):
+    """
+    Each letter in the plain text is replaced by another letter at some fixed positions from the current letter in the alphabet.
+    
+    Prameters
+    ---------
+    text : string that you want to encrypt or decrypt
+    shift: the number of positions the letter want to move to
+    encrypt: True if you want to encrypt the string and False if you want to decrypt it.
+    
+    Returns
+    -------
+    Newly replaced string
+    
+    Examples
+    --------
+    Input: cipher('hello', 1, True)
+    Output: 'ifmmp'
+    
+    Input: cipher('hello', 1, False)
+    Output: 'gdkkn'
+    
+    """
     alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     new_text = ''
     for c in text:
