776bb23fa7e5f2a715d26ed7be30dbaadf30a3c4
Alatas <ha2573@cumc.columbia.edu>
2021-11-09 16:16:35 -0500

    'newpoetry'

---
+++ cipher_hs3158/jupyter_execute/example.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Example usage
+# 
+# To use `cipher_hs3158` in a project:
+
+# In[1]:
+
+
+import cipher_hs3158
+
+print(cipher_hs3158.__version__)
+
--- cipher_hs3158/tests/test_cipher_hs3158.py
+++ cipher_hs3158/tests/test_cipher_hs3158.py
@@ -1,1 +1,25 @@
 from cipher_hs3158 import cipher_hs3158
+
+import pytest
+
+def cipher(text, shift, encrypt=True):
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    new_text = ''
+    for c in text:
+        index = alphabet.find(c)
+        if index == -1:
+            new_text += c
+        else:
+            new_index = index + shift if encrypt == True else index - shift
+            new_index %= len(alphabet)
+            new_text += alphabet[new_index:new_index+1]
+    return new_text
+
+
+@pytest.mark.parametrize('example, expected',[
+    ('hello', 'ifmmp')
+])
+
+def test_cipher(example, expected):
+    result = cipher("hello", 1, True)
+    assert result == expected
